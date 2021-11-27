096a99d2ca1b189b990d0f5d4ab53f05e262edad
Nazarii Sadovyi <nazarii.sadovyi@eleks.com>
2021-11-27 17:37:57 +0200

    Update video loading and unloading behaviour.

--- Client/FST.Client/Views/GridFilePreviewView.xaml.cs
+++ Client/FST.Client/Views/GridFilePreviewView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using FST.Client.Helpers;
+using FST.ViewModel.ViewModels;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
 
@@ -9,9 +13,37 @@ namespace FST.Client.Views
     /// </summary>
     public partial class GridFilePreviewView : UserControl
     {
+        private MediaElement VideoPlayer => this.FindVisualChildren<MediaElement>().FirstOrDefault();
+
         public GridFilePreviewView()
         {
             InitializeComponent();
+
+            var viewModel = DataContext as GridFilePreviewViewModel;
+            viewModel.SharedAppDataViewModel.PropertyChanged += SharedAppDataViewModel_PropertyChanged;
+        }
+
+        private void SharedAppDataViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(SharedAppDataViewModel.IsPreviewVisible):
+                    Application.Current.Dispatcher.Invoke(() => 
+                    {
+                        var viewModel = DataContext as GridFilePreviewViewModel;
+                        if (!viewModel.SharedAppDataViewModel.IsPreviewVisible)
+                        {
+                            VideoPlayer?.Stop();
+                        }
+                        else
+                        {
+                            VideoPlayer?.Play();
+                        }
+                    });
+                    break;
+                default:
+                    break;
+            }
         }
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
@@ -19,6 +51,16 @@ namespace FST.Client.Views
             var hwndSource = PresentationSource.FromVisual(this) as HwndSource;
             var hwndTarget = hwndSource.CompositionTarget;
             hwndTarget.RenderMode = RenderMode.SoftwareOnly;
+        }
+
+        private void MediaElement_Loaded(object sender, RoutedEventArgs e)
+        {
+            VideoPlayer?.Play();
+        }
+
+        private void MediaElement_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            VideoPlayer?.Pause();
         }
     }
 }
--- Client/FST.ViewModel/Interfaces/ISharedAppDataViewModel.cs
+++ Client/FST.ViewModel/Interfaces/ISharedAppDataViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System.Windows.Media.Imaging;
+﻿using System.ComponentModel;
+using System.Windows.Media.Imaging;
 
 namespace FST.ViewModel.ViewModels.Interfaces
 {
     public interface ISharedAppDataViewModel
     {
+        event PropertyChangedEventHandler PropertyChanged;
         bool IsActivated { get; set; }
         bool IsPreviewVisible { get; set; }
         BitmapImage WifiQRImage { get; set; }
